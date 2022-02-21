0b59646d70726ae22364d89b7110085260421561
shijiel <shijiel@umass.edu>
2022-02-20 22:44:27 -0500

    optimize merge sort2

--- src/laioffer/MergeSort.java
+++ src/laioffer/MergeSort.java
@@ -41,10 +41,46 @@ public class MergeSort {
 //        }
 
     }
+
+    public void mergeSort2(int[] array, int[] helper, int start, int end) {
+        if (array.length == 1 || array.length ==0) {
+            return;
+        }
+        int k = 0;
+        for (int i = 0; i < end - start + 1; i++) {
+            helper[i] = array[i];
+        }
+
+        int mid = start + (end - start - 1)/2;
+        int i = start;
+        int j = mid + 1;
+
+        mergeSort2(array, helper, start, mid);
+        mergeSort2(array, helper, mid + 1, end);
+
+        while(i <= mid && j <= end) {
+            if (array[i] <= array[j]) {
+                helper[k++] = array[i++];
+            } else {
+                helper[k++] = array[j++];
+            }
+        }
+        while (i <= mid) {
+            helper[k++] = array[i++];
+        }
+        while (j <= end) {
+            helper[k++] = array[j++];
+        }
+        for (int t = start; t < end; t++) {
+            array[t++] = helper[t++];
+        }
+
+    }
     public static void main(String[] args) {
         int[] arr = {3, 2, 1, 3, 7, 1, 2, 9, 0, 7, 8};
         MergeSort mergeSort = new MergeSort();
-        mergeSort.mergeSort(arr, 0, arr.length);
+        int[] helper = new int[arr.length];
+        mergeSort.mergeSort2(arr, helper,0, arr.length);
         System.out.println(arr);
     }
 }
