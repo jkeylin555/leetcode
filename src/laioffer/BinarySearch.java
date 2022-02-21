272950a397ce9faa2c82c30b4c36dc2196421e54
shijiel <shijiel@umass.edu>
2022-02-21 11:35:07 -0500

    k closest targe number

--- src/laioffer/BinarySearch.java
+++ src/laioffer/BinarySearch.java
@@ -1,6 +1,9 @@
 package laioffer;
 
+import java.util.Arrays;
+
 public class BinarySearch {
+    //find the target number and return the index of the target number
     public int binarySearch(int[] input, int left, int right, int target) {
         if (input == null || right - left < 0) {
             return -1;
@@ -17,6 +20,7 @@ public class BinarySearch {
         return leftIndex >= 0 ? leftIndex:rightIndex;
     }
 
+    //find the left most target number
     public int binarySearch2(int[] input, int left, int right, int target) {
         if (input == null || right - left < 0) {
             return -1;
@@ -40,11 +44,86 @@ public class BinarySearch {
         }
         return leftIndex >= 0 ? leftIndex:rightIndex;
     }
+
+    //find the closest number of target and return the index of that number
+    public int binarySearch3(int[] input, int left, int right, int target) {
+        if (input == null || right - left < 0) {
+            return -1;
+        }
+        int mid = left + (right - left)/2;
+        if (right - left <= 1) {
+            return Math.abs(input[right] - target) < Math.abs(input[left] - target)? right:left;
+        }
+
+        int leftIndex = -1, rightIndex = -1;
+        if (input[mid] >= target) {
+            leftIndex = binarySearch3(input, left, mid, target);
+        } else {
+            rightIndex = binarySearch3(input, mid, right, target);
+        }
+        return leftIndex >= 0 ? leftIndex:rightIndex;
+    }
+
+    //find the closest k numbers of target number
+    public int binarySearch4(int[] input, int left, int right, int target) {
+        if (input == null || right - left < 0) {
+            return -1;
+        }
+        int mid = left + (right - left)/2;
+        if (right - left <= 1) {
+            return Math.abs(input[right] - target) < Math.abs(input[left] - target)? right:left;
+        }
+
+        int leftIndex = -1, rightIndex = -1;
+        if (input[mid] >= target) {
+            leftIndex = binarySearch4(input, left, mid, target);
+        } else {
+            rightIndex = binarySearch4(input, mid, right, target);
+        }
+        return leftIndex >= 0 ? leftIndex:rightIndex;
+    }
+    public int[] kClosestNumber(int[] input, int left, int right, int target, int k) {
+        if (input == null || k <= 0) {
+            return new int[]{-1};
+        }
+        int[] result = new int[k];
+        int targeIndex = binarySearch4(input, left, right, target);
+        int m = targeIndex, n = targeIndex;
+        if (k >= input.length) {
+            for(int i = 0; i < input.length; i++) {
+                result[i] = i;
+            }
+        } else if (k == 1) {
+            return new int[] {targeIndex};
+        } else {
+            while (k > 1 && m > left && n < right) {
+                k--;
+                if (Math.abs(input[m-1] - target) <= Math.abs(input[n+1] - target)) {
+                    m--;
+                } else n++;
+            }
+            while (k > 1 && m> left) {
+                m--;
+                k--;
+            }
+            while (k > 1 && n < right) {
+                n++;
+                k--;
+            }
+            for (int i = m; i <= n; i++) {
+                result[i - m] = i;
+            }
+        }
+        return  result;
+    }
+
+
+
     public static void main(String[] args) {
         int[] arr = new int[]{1, 1, 1, 3, 3, 5, 6, 8, 11, 20};
         BinarySearch binarySearch = new BinarySearch();
-        int targetIndex = binarySearch.binarySearch2(arr, 0, arr.length - 1, 1);
-        System.out.println(targetIndex);
+        int[] targetArray = binarySearch.kClosestNumber(arr, 0, arr.length - 1, 10, 3);
+        System.out.println(Arrays.toString(targetArray));
 
     }
 
