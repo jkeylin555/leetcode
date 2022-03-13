d732994d9bf30d97c65e3786d051234971849431
jkeylin555 <shijiel@umass.edu>
2022-03-13 11:04:15 -0400

    removeDuplicate3

--- src/exercise/FastSlowPointers/RemoveDuplicates.java
+++ src/exercise/FastSlowPointers/RemoveDuplicates.java
@@ -56,7 +56,26 @@ public class RemoveDuplicates {
 		return array;
 	}
 
-
+    private int[] removeDuplicates3(int[] array) {
+		int slow = 0, fast = 0, counter = 1;
+		while (fast < array.length - 1) {
+			if (array[fast] == array[fast + 1]) {
+				fast++;
+				counter++;
+			} else {
+				if (counter > 1) {
+					fast++;
+					counter = 1;
+				} else {
+					array[slow++] = array[fast++];
+				}
+				if (fast == array.length - 1) {
+					array[slow++] = array[fast++];
+				}
+			}
+		}
+		return array;
+	}
 
 
 
@@ -64,8 +83,10 @@ public class RemoveDuplicates {
 		int[] array = {0,0,1,1,1,2,2,3,3,4};
 		int[] array2 = {1,1,1,2,2,3};
 		int[] array3 = {1,1,1,1,2,2,3,3};
+		int[] array4 = {1,1,2,3,3};
+		int[] array5 = {1,1,2,3,4,4,4,5};
 		RemoveDuplicates removeDuplicates = new RemoveDuplicates();
-		int[] result = removeDuplicates.removeDuplicates2_2(array3);
+		int[] result = removeDuplicates.removeDuplicates3(array4);
 		System.out.println(Arrays.toString(result));
 	}
 
