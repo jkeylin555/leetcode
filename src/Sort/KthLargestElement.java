ae3f85848a861fd1be1b177efb02db90e2a96beb
jkeylin555 <shijiel@umass.edu>
2022-03-28 18:29:58 -0400

    215. Kth Largest Element in an Array

--- src/Sort/KthLargestElement.java
+++ src/Sort/KthLargestElement.java
@@ -1,22 +1,56 @@
 package Sort;
 
 import java.util.PriorityQueue;
+import java.util.Random;
 
 public class KthLargestElement {
 	public int findKthLargest(int[] nums, int k) {
-		PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-		if (k > nums.length) {
-			return -1;
+		return quickSort(nums, 0, nums.length - 1, k);
+	}
+	public int quickSort(int[] nums, int left, int right, int k) {
+		int index = randomPartition(nums, left, right);
+		if (index == k - 1) {
+			return nums[index];
+		} else if (index > k - 1) {
+			return quickSort(nums, left, index - 1, k);
+		} else {
+			return quickSort(nums, index + 1, right, k);
 		}
-		for (int i = 0; i < k; i++) {
-			minHeap.offer(nums[i]);
-		}
-		for (int i = k; i < nums.length; i++) {
-			if (nums[i] > minHeap.peek()) {
-				minHeap.poll();
-				minHeap.offer(nums[i]);
+	}
+	public int randomPartition(int[] nums, int left, int right) {
+		Random rand = new Random();
+		int random = rand.nextInt(right - left + 1) + left;
+		swap(nums, random, right);
+		return partion(nums, left, right);
+	}
+	private int partion(int[] nums, int left, int right) {
+		int pivot = right;
+		while (left <= right) {
+			while (left <= right && nums[right] <= nums[pivot]) {
+				right--;
+			}
+			while (left <= right && nums[left] > nums[pivot]) {
+				left++;
+			}
+			if (right >= left) {
+				swap(nums, left, right);
+				left++;
+				right--;
 			}
 		}
-		return minHeap.peek();
+		swap(nums, right + 1, pivot);
+		return right + 1;
+	}
+	private void swap(int[] nums, int a, int b) {
+		int temp = nums[a];
+		nums[a] = nums[b];
+		nums[b] = temp;
+	}
+	public static void main(String[] args) {
+		int[] nums = {3,2,1,5,6,4};
+		int[] nums2 = {3,2,1,4,6,5};
+		KthLargestElement kthLargestElement = new KthLargestElement();
+//		kthLargestElement.partion(nums2, 0 , 5);
+		System.out.println(kthLargestElement.findKthLargest(nums, 3));
 	}
 }
