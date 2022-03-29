2fef367365c7e78528be30c76f5f16efadc0c32a
jkeylin555 <shijiel@umass.edu>
2022-03-29 13:36:11 -0400

    majority3

--- src/Math/MajorityElement.java
+++ src/Math/MajorityElement.java
@@ -1,7 +1,9 @@
 package Math;
 
 import Stack.TrappingRainWater;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MajorityElement {
@@ -28,8 +30,7 @@ public class MajorityElement {
 		for (int i = 0; i < nums.length; i++) {
 			if (count == 0) {
 				major = nums[i];
-			}
-			if (nums[i] == major) {
+			} else if (nums[i] == major) {
 				count++;
 			} else {
 				count--;
@@ -38,14 +39,55 @@ public class MajorityElement {
 		return major;
 	}
 
+	public List<Integer> majority3(int[] nums) {
+		List<Integer> res = new ArrayList<>();
+		int major1 = -1, major2 = -1, count1 = 0, count2 = 0;
+		for (int i = 0; i < nums.length; i++) {
+
+			if (major1 == nums[i]) {
+				count1++;
+			} else if (major2 == nums[i]) {
+				count2++;
+			}else if (count1 == 0) {
+				major1 = nums[i];
+				count1++;
+			} else if (count2 == 0) {
+				major2 = nums[i];
+				count2++;
+			} else {
+				count1--;
+				count2--;
+			}
+
+		}
+		count1 = 0;count2 = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == major1) {
+				count1++;
+			}
+			if (nums[i] == major2) {
+				count2++;
+			}
+		}
+		if ((float)count1/nums.length > (float)1/3) {
+			res.add(major1);
+		}
+
+		if ((float)count2/nums.length > (float)1/3 && major1 != major2) {
+			res.add(major2);
+		}
+		return res;
+	}
+
 
 
 
 	public static void main(String[] args) {
-		int[] duNums = {2,2,1,1,1,2,2};
+		int[] duNums = {2,1,1,3,1,4,5,6};
 		int[] duNums2 = {3,2,3};
 		MajorityElement majorityElement = new MajorityElement();
-		System.out.println(majorityElement.majority2(duNums2));
+		System.out.println(majorityElement.majority3(duNums));
+//		System.out.println((float)(1/3));
 //		Map<String,Integer> map = new HashMap<>();
 //		map.put("aaa", 1);
 //		Map.Entry<String,Integer> entry = map.entrySet().iterator().next();
