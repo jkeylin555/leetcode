54e58b260267e1b44f103a14eb5e2dc2709eb354
jkeylin555 <shijiel@umass.edu>
2022-03-28 21:26:33 -0400

    4. Median of Two Sorted Arrays

---
+++ src/Search/MedianOfTwoArrays.java
@@ -0,0 +1,42 @@
+package Search;
+
+import Sort.KthLargestElement;
+
+public class MedianOfTwoArrays {
+	public int findMedianSortedArrays(int[] nums1, int left1,  int[] nums2, int left2, int k) {
+		if (left1 > nums1.length - 1) {
+			return nums2[left2 + k - 1];
+		}
+		if (left2 > nums2.length - 1) {
+			return nums1[left1 + k - 1];
+		}
+		if (k == 1) {
+			return nums1[left1] < nums2[left2] ? nums1[left1] : nums2[left2];
+		}
+
+		if (left1 + k/2 - 1> nums1.length - 1) {
+			return findMedianSortedArrays(nums1, left1, nums2, left2 + k/2, k - k/2);
+		}
+		if (left2 + k/2 - 1> nums2.length - 1) {
+			return findMedianSortedArrays(nums1, left1 + k/2, nums2, left2 , k - k/2);
+		}
+
+//		if (nums1[left1 + k/2 - 1] == nums2[left2 + k/2 - 1]) {
+//			return nums1[left1 + (k - 1)/2];
+//		}
+//		else
+			if (nums1[left1 + k/2 - 1] < nums2[left2 + k/2 - 1]) {
+			return findMedianSortedArrays(nums1, left1 + k/2, nums2, left2, k - k/2);
+		} else {
+			return findMedianSortedArrays(nums1, left1, nums2, left2 + k/2, k - k/2);
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] nums1 = {1};
+		int[] nums2 = {2,3,4};
+		MedianOfTwoArrays medianOfTwoArrays = new MedianOfTwoArrays();
+//		kthLargestElement.partion(nums2, 0 , 5);
+		System.out.println(medianOfTwoArrays.findMedianSortedArrays(nums1, 0, nums2, 0, 3));
+	}
+}
