6f26ad540154ff83d6d8248d0f25742f30bdd6b0
jkeylin555 <shijiel@umass.edu>
2022-04-05 15:50:46 -0400

    reverseWord

--- src/String/ReverseYahool.java
+++ src/String/ReverseYahool.java
@@ -1,12 +1,19 @@
 package String;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import recursion.ReverseString;
 
 public class ReverseYahool {
 	public String reverse(String str) {
 		ReverseString reverseString = new ReverseString();
+		StringBuilder result = new StringBuilder();
 		char[] chars = str.toCharArray();
-		int i = 0, j = 0;
+		int i = 0, j = 0, k = 0;
+		while (chars[k] == ' ' && k < chars.length) {
+			k++;
+		}
+		i = k;
 		while (j < chars.length) {
 			if (j == chars.length - 1)  {
 				reverseString.reverse(chars, i, j);
@@ -23,12 +30,88 @@ public class ReverseYahool {
 		return String.valueOf(chars);
 	}
 
+	public String reverseWords(String s) {
+		StringBuilder word = new StringBuilder();
+		Deque<String> stack = new ArrayDeque<>();
+		int i = 0, j = s.length() - 1;
+		while (i < s.length() && s.charAt(i) == ' ') {
+			i++;
+		}
+		while (j >= 0 && s.charAt(j) == ' ') {
+			j--;
+		}
+		while (i <= j) {
+		if (word.length() != 0 && s.charAt(i) == ' ' || i == j) {
+				stack.push(word.toString());
+				word.setLength(0);
+				i++;
+			} else if (s.charAt(i) != ' '){
+				word.append(s.charAt(i));
+				i++;
+			} else {
+			i++;
+		    }
+		}
+		return String.join(" ", stack);
+	}
+
+
+	public String reverseWords2(String s) {
+		StringBuilder sb = trimSpaces(s);
+		reverse(sb, 0, sb.length() - 1);
+		int i = 0, j = 0;
+		while (j < sb.length()) {
+			if (sb.charAt(j) == ' ' || j == sb.length() - 1) {
+				reverse(sb, i, j - 1);
+				i = ++j;
+			} else {
+				++j;
+			}
+		}
+		return sb.toString();
+
+	}
+	private void reverse (StringBuilder sb, int left, int right) {
+		if (left >= right) return;;
+		swap(sb, left, right);
+		reverse(sb, ++left, --right);
+	}
+
+	private void swap (StringBuilder sb, int left, int right) {
+		char temp = sb.charAt(left);
+		sb.setCharAt(left, sb.charAt(right));
+		sb.setCharAt(right, temp);
+	}
+
+
+	public StringBuilder trimSpaces(String s) {
+		StringBuilder words = new StringBuilder();
+		int i = 0, j = s.length() - 1;
+		while (i < s.length() - 1 && s.charAt(i) == ' ') {
+			i++;
+		}
+		while (j > i && s.charAt(j) == ' ') {
+			j--;
+		}
+		while (i <= j) {
+			char c = s.charAt(i);
+			if (c != ' ') {
+				words.append(c);
+			} else if (words.charAt(words.length() - 1) != ' ' && c == ' ') {
+				words.append(c);
+			}
+			++i;
+		}
+		return words;
+	}
+
+
 	public static void main(String[] args) {
 		String str = "a";
 		String str2 = "a";
-		String str3 = "I love yahool";
+		String str3 = "the sky is blue";
 		ReverseYahool reverseYahool = new ReverseYahool();
-		String res = reverseYahool.reverse(str3);
+		String res = reverseYahool.reverseWords2(str3);
 		System.out.println(res);
 
 	}
--- src/laioffer/JavaTest.java
+++ src/laioffer/JavaTest.java
@@ -1,5 +1,7 @@
 package laioffer;
 
+import java.util.Arrays;
+
 public class JavaTest {
     public static class LaiOfferManager {
         String name;
@@ -45,15 +47,45 @@ public class JavaTest {
         // this will fail, as there is no *this* in a static context
     }
 
+    public static double median(double ... numbers) {
+        Arrays.sort(numbers);
+
+        int size = numbers.length;
+        double median = 0.0;
+
+        if (size > 0) {
+            if (size % 2 == 0) {
+                double a = numbers[size >> 1];
+                double b = numbers[(size >> 1) - 1];
+                double c = numbers[size >> 1] + numbers[(size >> 1) - 1];
+                median = (numbers[size >> 1] + numbers[(size >> 1) - 1])/2.0;
+            }
+            else {
+                median = numbers[size >> 1];
+            }
+        }
+
+        return median;
+    }
+
     public static void main(String[] args) {
         LaiOfferManager sun = new LaiOfferManager();
         sun.name = "sun";
         sun.salary = 10000;
         LaiOfferManager.ipo();
-        System.out.println(LaiOfferManager.isRich);
+//        System.out.println(LaiOfferManager.isRich);
         new JavaTest().D();
         B b = new B();
         b.a();
         new B().a();
+        String s = "  hello ";
+        s.trim();
+       //1 4 7 9 11 21
+        double[] numbers = {9, 11, 1, 4, 7, 21};
+
+        JavaTest javaTest = new JavaTest();
+        double res = javaTest.median(numbers);
+
+        System.out.println(res);
     }
 }
