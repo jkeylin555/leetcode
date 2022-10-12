206b555ea73826dac4008232c7f026eff5776c2e
jkeylin555 <shijiel@umass.edu>
2022-10-11 20:54:00 -0400

    add C and D

--- src/laioffer/test.java
+++ src/laioffer/test.java
@@ -11,3 +11,20 @@ class A {
         c = 'a';
     }
 }
+
+class C {
+    int a;
+    char c;
+    public C() {
+        a = 1;
+        c = 'a';
+    }
+}
+class D {
+    int a;
+    char c;
+    public D() {
+        a = 1;
+        c = 'a';
+    }
+}
